07a4ad59b8ba18cd50f3f1d5f3ae71cc08b91c66
Fer Vittorello <fvittorello@gmail.com>
2020-08-29 05:25:44 -0300

    chore(maquetado) refactor on sections, margins and elements

---
+++ maquetado-frontend/index.js
@@ -0,0 +1,25 @@
+const yesterdayRadio = document.querySelector('#yesterday-radio');
+const todayRadio = document.querySelector('#today-radio');
+const tomorrowRadio = document.querySelector('#tomorrow-radio');
+
+const yesterdayItems = document.querySelector('#yesterday-items');
+const todayItems = document.querySelector('#today-items');
+const tomorrowItems = document.querySelector('#tomorrow-items');
+
+yesterdayRadio.addEventListener('click', () => {
+	toggleClassFromElements('display-none', yesterdayItems, [todayItems, tomorrowItems]);
+});
+
+todayRadio.addEventListener('click', () => {
+	toggleClassFromElements('display-none', todayItems, [yesterdayItems, tomorrowItems]);
+});
+tomorrowRadio.addEventListener('click', () => {
+	toggleClassFromElements('display-none', tomorrowItems, [yesterdayItems, todayItems]);
+});
+
+function toggleClassFromElements(className, starterElement, elements) {
+	starterElement.classList.remove(className);
+	elements.forEach((element) => {
+		element.classList.add(className);
+	});
+}
