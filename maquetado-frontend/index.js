b81c49f63cc754b234643c5c7dac761d920297d6
Fer Vittorello <fvittorello@gmail.com>
2020-08-29 20:10:06 -0300

    chore(maquetado) refactor of semantic elements, styles and paddings

--- maquetado-frontend/index.js
+++ maquetado-frontend/index.js
@@ -1,10 +1,11 @@
 const yesterdayRadio = document.querySelector('#yesterday-radio');
 const todayRadio = document.querySelector('#today-radio');
 const tomorrowRadio = document.querySelector('#tomorrow-radio');
-
 const yesterdayItems = document.querySelector('#yesterday-items');
 const todayItems = document.querySelector('#today-items');
 const tomorrowItems = document.querySelector('#tomorrow-items');
+const burgerCheckbox = document.querySelector('#hor-bars');
+const burgerIcon = document.querySelector('#burger-icon');
 
 yesterdayRadio.addEventListener('click', () => {
 	toggleClassFromElements('display-none', yesterdayItems, [todayItems, tomorrowItems]);
@@ -17,11 +18,28 @@ tomorrowRadio.addEventListener('click', () => {
 	toggleClassFromElements('display-none', tomorrowItems, [yesterdayItems, todayItems]);
 });
 
+burgerCheckbox.addEventListener('click', () => {
+	if (isChecked(burgerCheckbox)) {
+		toggleClassFromElements('bar-icon--checked', '', [burgerIcon]);
+	} else {
+		toggleClassFromElements('bar-icon--checked', burgerIcon, []);
+	}
+});
+
 function toggleClassFromElements(className, starterElement, elements) {
-	starterElement.classList.remove(className);
+	if (!!starterElement) {
+		starterElement.classList.remove(className);
+	}
 	elements.forEach((element) => {
 		element.classList.add(className);
 	});
+}
+
+function isChecked(checkbox) {
+	if (!!checkbox.checked) {
+		return true;
+	}
+	return false;
 }
 
 window.addEventListener('load', () => {
