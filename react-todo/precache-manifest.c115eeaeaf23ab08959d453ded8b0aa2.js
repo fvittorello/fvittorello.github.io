3ab0ff3c17bb2166271fd430a5586ccb2b446591
Fer Vittorello <fvittorello@gmail.com>
2020-11-21 15:28:47 -0300

    feat(react-todo) add project folder

---
+++ react-todo/precache-manifest.c115eeaeaf23ab08959d453ded8b0aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2360c9e335cfd919144cd18c21e0ab",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "471b1bbe7adcdcbb2d49",
+    "url": "/react-todo/static/css/main.5481848d.chunk.css"
+  },
+  {
+    "revision": "0fb81ad09c1ef56d52fa",
+    "url": "/react-todo/static/js/2.07d5ba12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.07d5ba12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471b1bbe7adcdcbb2d49",
+    "url": "/react-todo/static/js/main.82f5b738.chunk.js"
+  },
+  {
+    "revision": "df1dcdac991e1df66de0",
+    "url": "/react-todo/static/js/runtime-main.a9476ca2.js"
+  }
+]);
